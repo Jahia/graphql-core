1bd36afc6a4759994f85792116975f2254385132
Thomas Draier <tdraier@jahia.com>
2018-02-06 17:31:31 +0100

    BACKLOG-7179 : Added test for mutateNode , mutateNodes , mutateNodesByQuery

--- graphql-test/src/main/java/org/jahia/test/graphql/GraphQLNodeMutationsTest.java
+++ graphql-test/src/main/java/org/jahia/test/graphql/GraphQLNodeMutationsTest.java
@@ -51,43 +51,51 @@ import org.jahia.services.content.JCRSessionFactory;
 import org.jahia.services.content.JCRTemplate;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
+import javax.jcr.RepositoryException;
 import java.util.Locale;
 
 public class GraphQLNodeMutationsTest extends GraphQLTestSupport {
 
     @BeforeClass
     public static void oneTimeSetup() throws Exception {
-
         GraphQLTestSupport.init();
+    }
 
+    @Before
+    public void setup() throws Exception {
         JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.ENGLISH, session -> {
             JCRNodeWrapper node = session.getNode("/").addNode("testList", "jnt:contentList");
-            JCRNodeWrapper subNode = node.addNode("testSubList", "jnt:contentList");
+            JCRNodeWrapper subNode1 = node.addNode("testSubList1", "jnt:contentList");
+            JCRNodeWrapper subNode2 = node.addNode("testSubList2", "jnt:contentList");
             session.save();
             return null;
         });
     }
 
+    @After
+    public void tearDown() throws Exception {
+        GraphQLTestSupport.removeTestNodes();
+        JCRSessionFactory.getInstance().closeAllSessions();
+    }
 
     @Test
     public void addNode() throws Exception {
         JSONObject result = executeQuery("mutation {\n" +
                 "  jcr {\n" +
-                "    addNode(parentPathOrId:\"/testList\",name:\"testSubList2\",primaryNodeType:\"jnt:contentList\") {\n" +
+                "    addNode(parentPathOrId:\"/testList\",name:\"testNew\",primaryNodeType:\"jnt:contentList\") {\n" +
                 "      uuid\n" +
                 "    }\n" +
                 "  }\n" +
                 "}\n");
         String uuid = result.getJSONObject("data").getJSONObject("jcr").getJSONObject("addNode").getString("uuid");
-
-        JCRNodeWrapper node = JCRSessionFactory.getInstance().getCurrentUserSession().getNodeByIdentifier(uuid);
-        Assert.assertEquals("/testList/testSubList2", node.getPath());
-        Assert.assertTrue(node.isNodeType("jnt:contentList"));
+        JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.ENGLISH, session -> {
+            JCRNodeWrapper node = session.getNodeByIdentifier(uuid);
+            Assert.assertEquals("/testList/testNew", node.getPath());
+            assertNodeType(node, "jnt:contentList");
+            return null;
+        });
     }
 
     @Test
@@ -124,19 +132,81 @@ public class GraphQLNodeMutationsTest extends GraphQLTestSupport {
                 "  }\n" +
                 "}\n");
         JSONArray res = result.getJSONObject("data").getJSONObject("jcr").getJSONArray("addNodesBatch");
+        String uuid1 = res.getJSONObject(0).getString("uuid");
+        String uuid2 = res.getJSONObject(1).getString("uuid");
 
-        JCRNodeWrapper node1 = JCRSessionFactory.getInstance().getCurrentUserSession(null, Locale.ENGLISH).getNodeByIdentifier(res.getJSONObject(0).getString("uuid"));
-        Assert.assertEquals("/testList/testBatch1", node1.getPath());
-        Assert.assertTrue(node1.isNodeType("jnt:contentList"));
-        Assert.assertTrue(node1.isNodeType("jmix:renderable"));
-        Assert.assertEquals("test", node1.getProperty("jcr:title").getString());
-        JCRNodeWrapper node2 = JCRSessionFactory.getInstance().getCurrentUserSession().getNodeByIdentifier(res.getJSONObject(1).getString("uuid"));
-        Assert.assertEquals("/testList/testBatch2", node2.getPath());
+        JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.ENGLISH, session -> {
+            JCRNodeWrapper node1 = session.getNodeByIdentifier(uuid1);
+            Assert.assertEquals("/testList/testBatch1", node1.getPath());
+            assertNodeType(node1, "jnt:contentList");
+            assertNodeType(node1, "jmix:renderable");
+            assertPropertyValue(node1, "jcr:title", "test");
+            JCRNodeWrapper node2 = JCRSessionFactory.getInstance().getCurrentUserSession().getNodeByIdentifier(uuid2);
+            Assert.assertEquals("/testList/testBatch2", node2.getPath());
+            return null;
+        });
     }
 
-    @AfterClass
-    public static void oneTimeTearDown() throws Exception {
-        GraphQLTestSupport.removeTestNodes();
+    @Test
+    public void mutateNode() throws Exception {
+        JSONObject result = executeQuery("mutation {\n" +
+                "  jcr {\n" +
+                "    mutateNode(pathOrId: \"/testList/testSubList1\") {\n" +
+                "      mutateProperty(name: \"jcr:title\") {\n" +
+                "        setValue(language: \"en\", value: \"test\")\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n");
+        JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.ENGLISH, session -> {
+            assertPropertyValue(session.getNode("/testList/testSubList1"), "jcr:title", "test");
+            return null;
+        });
     }
+
+    @Test
+    public void mutateNodes() throws Exception {
+        JSONObject result = executeQuery("mutation {\n" +
+                "  jcr {\n" +
+                "    mutateNodes(pathsOrIds: [\"/testList/testSubList1\",\"/testList/testSubList2\"]) {\n" +
+                "      mutateProperty(name: \"jcr:title\") {\n" +
+                "        setValue(language: \"en\", value: \"test\")\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n");
+        JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.ENGLISH, session -> {
+            assertPropertyValue(session.getNode("/testList/testSubList1"), "jcr:title", "test");
+            assertPropertyValue(session.getNode("/testList/testSubList2"), "jcr:title", "test");
+            return null;
+        });
+    }
+
+    @Test
+    public void mutateNodesByQuery() throws Exception {
+        JSONObject result = executeQuery("mutation {\n" +
+                "  jcr {\n" +
+                "    mutateNodesByQuery(query:\"select * from [jnt:contentList] where isdescendantnode('/testList')\",queryLanguage:SQL2) {\n" +
+                "      mutateProperty(name: \"jcr:title\") {\n" +
+                "        setValue(language: \"en\", value: \"test\")\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n");
+        JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.ENGLISH, session -> {
+            assertPropertyValue(session.getNode("/testList/testSubList1"), "jcr:title", "test");
+            assertPropertyValue(session.getNode("/testList/testSubList2"), "jcr:title", "test");
+            return null;
+        });
+    }
+
+    private void assertPropertyValue(JCRNodeWrapper node, String property, String value) throws RepositoryException {
+        Assert.assertEquals(value, node.getProperty(property).getString());
+    }
+
+    private void assertNodeType(JCRNodeWrapper node1, String typeName) throws RepositoryException {
+        Assert.assertTrue(node1.isNodeType(typeName));
+    }
+
 
 }
