96df6b9248ae1ec56190b85040846a6e66e32171
Thomas Draier <tdraier@jahia.com>
2018-03-05 13:32:15 +0100

    BACKLOG-7689 : Workflow extension for gql

---
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/workflow/WorkflowMutationExtensions.java
@@ -0,0 +1,73 @@
+/*
+ *  ==========================================================================================
+ *  =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ *  ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ *      Copyright (C) 2002-2017 Jahia Solutions Group SA. All rights reserved.
+ *
+ *      THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *      1/GPL OR 2/JSEL
+ *
+ *      1/ GPL
+ *      ==================================================================================
+ *
+ *      IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *      2/ JSEL - Commercial and Supported Versions of the program
+ *      ===================================================================================
+ *
+ *      IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *      Alternatively, commercial and supported versions of the program - also known as
+ *      Enterprise Distributions - must be used in accordance with the terms and conditions
+ *      contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *      If you are unsure which license is appropriate for your use,
+ *      please contact the sales department at sales@jahia.com.
+ *
+ */
+
+package org.jahia.modules.graphql.provider.dxm.workflow;
+
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLTypeExtension;
+import org.apache.commons.lang.StringUtils;
+import org.jahia.modules.graphql.provider.dxm.DXGraphQLProvider;
+import org.jahia.osgi.BundleUtils;
+import org.jahia.services.workflow.Workflow;
+import org.jahia.services.workflow.WorkflowService;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Extensions for JCRNodeMutation
+ */
+@GraphQLTypeExtension(DXGraphQLProvider.Mutation.class)
+public class WorkflowMutationExtensions {
+
+    @GraphQLField
+    public static Collection<GqlWorkflowMutation> mutateWorkflows(@GraphQLName("definition") String workflowDefinitionId) {
+        WorkflowService service = BundleUtils.getOsgiService(WorkflowService.class, null);
+        List<Workflow> wfs = service.getWorkflowsForDefinition(StringUtils.substringAfter(workflowDefinitionId,":"), null);
+        return wfs.stream().map(GqlWorkflowMutation::new).collect(Collectors.toList());
+    }
+}
