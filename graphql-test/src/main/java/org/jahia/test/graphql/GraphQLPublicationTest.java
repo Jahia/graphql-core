fe88a4d77fe1e11fc7860d75c8830a075768af90
oyevstafyev <oievstafiev@jahia.com>
2018-03-21 17:17:03 +0100

    [BACKLOG-7598] Integration tests added

---
+++ graphql-test/src/main/java/org/jahia/test/graphql/GraphQLPublicationTest.java
@@ -0,0 +1,116 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2018 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.test.graphql;
+
+import java.util.Locale;
+
+import org.jahia.api.Constants;
+import org.jahia.services.content.JCRTemplate;
+import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GraphQLPublicationTest extends GraphQLTestSupport {
+
+    @BeforeClass
+    public static void oneTimeSetup() throws Exception {
+
+        GraphQLTestSupport.init();
+
+        JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.ENGLISH, session -> {
+            session.getNode("/").addNode("testList", "jnt:contentList");
+            session.save();
+            return null;
+        });
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() throws Exception {
+        GraphQLTestSupport.removeTestNodes();
+    }
+
+    @Test
+    public void shouldRetrieveAggregatedPublicationInfoFromDefault() throws Exception {
+
+        JSONObject result = executeQuery(""
+                + "{"
+                + "    jcr {"
+                + "        nodeByPath(path: \"/testList\") {"
+                + "            aggregatedPublicationInfo(language: \"en\") {"
+                + "                publicationStatus"
+                + "                locked"
+                + "                workInProgress"
+                + "                allowedToPublishWithoutWorkflow"
+                + "		       }"
+                + "        }"
+                + "    }"
+                + "}");
+
+        JSONObject info = result.getJSONObject("data").getJSONObject("jcr").getJSONObject("nodeByPath").getJSONObject("aggregatedPublicationInfo");
+
+        Assert.assertEquals("NOT_PUBLISHED", info.getString("publicationStatus"));
+        Assert.assertFalse(info.getBoolean("locked"));
+        Assert.assertFalse(info.getBoolean("workInProgress"));
+        Assert.assertTrue(info.getBoolean("allowedToPublishWithoutWorkflow"));
+    }
+
+    @Test
+    public void shouldGetErrorNotRetrieveAggregatedPublicationInfoFromLive() throws Exception {
+
+        JSONObject result = executeQuery(""
+                + "{"
+                + "    jcr(workspace: LIVE) {"
+                + "        nodeByPath(path: \"/\") {"
+                + "            aggregatedPublicationInfo(language: \"en\") {"
+                + "                publicationStatus"
+                + "		       }"
+                + "        }"
+                + "    }"
+                + "}");
+
+        validateError(result, "Publication fields can only be used with nodes from EDIT workspace");
+    }
+}
