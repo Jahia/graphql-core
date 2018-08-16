c167e2dba5917cc0c7611d790a556df568c62789
oyevstafyev <oievstafiev@jahia.com>
2018-08-16 16:37:58 +0200

    Revert "[BACKLOG-8302] Avoid unnecessary conversion of node type list"
    This reverts commit 48800cea4b50f4e02f91011525d7b8d093f7df48.

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/nodetype/NodetypeJCRNodeExtensions.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/nodetype/NodetypeJCRNodeExtensions.java
@@ -43,8 +43,10 @@
  */
 package org.jahia.modules.graphql.provider.dxm.nodetype;
 
+import com.google.common.base.Splitter;
 import graphql.annotations.annotationTypes.*;
 import graphql.schema.DataFetchingEnvironment;
+import org.apache.commons.lang.StringUtils;
 import org.jahia.modules.graphql.provider.dxm.node.GqlJcrNode;
 import org.jahia.modules.graphql.provider.dxm.node.NodeHelper;
 import org.jahia.modules.graphql.provider.dxm.predicate.FieldEvaluator;
@@ -58,7 +60,6 @@ import pl.touk.throwing.ThrowingFunction;
 import javax.jcr.RepositoryException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -126,14 +127,14 @@ public class NodetypeJCRNodeExtensions {
     @GraphQLDescription("Returns a list of types allowed under the provided node")
     public List<GqlJcrNodeType> getAllowedChildNodeTypes(@GraphQLName("fieldFilter") @GraphQLDescription("Filter by GraphQL fields values") FieldFiltersInput fieldFilter, DataFetchingEnvironment environment) {
 
-        Set<String> nodeTypes;
+        String constraints;
         try {
-            nodeTypes = ConstraintsHelper.getConstraintSet(node.getNode());
+            constraints = ConstraintsHelper.getConstraints(node.getNode());
         } catch (RepositoryException e) {
             throw new RuntimeException(e);
         }
 
-        return nodeTypes.stream().map(ThrowingFunction.unchecked(nodeType -> NodeTypeRegistry.getInstance().getNodeType(nodeType)))
+        return StringUtils.isEmpty(constraints) ? null : Splitter.on(" ").splitToList(constraints).stream().map(ThrowingFunction.unchecked(type -> NodeTypeRegistry.getInstance().getNodeType(type)))
                 .map(GqlJcrNodeType::new)
                 .filter(FilterHelper.getFieldPredicate(fieldFilter, FieldEvaluator.forList(environment)))
                 .collect(Collectors.toList());
