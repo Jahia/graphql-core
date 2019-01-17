665959fb742be57b8ae150f3a87b1b7c95b9e58c
tdraier <tdraier@jahia.com>
2019-01-17 13:47:51 +0100

    BACKLOG-8991 : Fixed invalid runtime directive wiring, enhance existing type and do not recreate it

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/sdl/parsing/SDLDirectiveWiring.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/sdl/parsing/SDLDirectiveWiring.java
@@ -1,61 +1,40 @@
 package org.jahia.modules.graphql.provider.dxm.sdl.parsing;
 
-import graphql.language.*;
-import graphql.schema.*;
+import graphql.schema.GraphQLDirective;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLObjectType;
 import graphql.schema.idl.SchemaDirectiveWiring;
 import graphql.schema.idl.SchemaDirectiveWiringEnvironment;
-import graphql.schema.idl.SchemaDirectiveWiringEnvironmentImpl;
 import org.jahia.modules.graphql.provider.dxm.sdl.fetchers.Field;
-import org.jahia.modules.graphql.provider.dxm.sdl.fetchers.PropertiesDataFetcher;
 import org.jahia.modules.graphql.provider.dxm.sdl.fetchers.PropertiesDataFetcherFactory;
-
-import static graphql.Scalars.GraphQLString;
 
 public class SDLDirectiveWiring implements SchemaDirectiveWiring {
 
     @Override
     public GraphQLObjectType onObject(SchemaDirectiveWiringEnvironment<GraphQLObjectType> environment) {
+        GraphQLObjectType.Builder builder = GraphQLObjectType.newObject(environment.getElement());
+
         GraphQLDirective directive = environment.getDirective();
         if (directive != null && directive.getName().equals("description")) {
-            String description = directive.getArgument("value").getValue().toString();
-            GraphQLObjectType obj = environment.getElement();
-            GraphQLObjectType.Builder builder = GraphQLObjectType.newObject()
-                    .name(obj.getName())
-                    .description(description)
-                    .fields(obj.getFieldDefinitions())
-                    .definition(obj.getDefinition());
-
-            for (GraphQLDirective d : obj.getDirectives()) {
-                builder.withDirective(d);
-            }
-            for (GraphQLOutputType i : obj.getInterfaces()) {
-                builder.withInterface((GraphQLInterfaceType) i);
-            }
-
-            return builder.build();
+            builder.description(environment.getDirective().getArgument("value").getValue().toString());
         }
-        return environment.getElement();
+        return builder.build();
     }
 
     @Override
     public GraphQLFieldDefinition onField(SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
-        Field field = new Field(environment.getElement().getName());
 
         GraphQLFieldDefinition def = environment.getElement();
 
         //TODO add arguments from factory as well based on type
-        GraphQLFieldDefinition.Builder builder = GraphQLFieldDefinition.newFieldDefinition()
-                .name(def.getName())
-                .dataFetcher(PropertiesDataFetcherFactory.getFetcher(def, field))
-                .argument(GraphQLArgument.newArgument().name("language").type(GraphQLString).build())
-                .withDirectives(def.getDirectives().toArray(new GraphQLDirective[0]))
-                .type(def.getType());
+        GraphQLFieldDefinition.Builder builder = GraphQLFieldDefinition.newFieldDefinition(def);
 
         GraphQLDirective directive = environment.getDirective();
         if (directive != null && directive.getName().equals("mapping")) {
+            Field field = new Field(environment.getElement().getName());
             field.setProperty(environment.getDirective().getArgument("property").getValue().toString());
-        }
-        else if (directive != null && directive.getName().equals("description")) {
+            builder.dataFetcher(PropertiesDataFetcherFactory.getFetcher(def, field));
+        } else if (directive != null && directive.getName().equals("description")) {
             builder.description(environment.getDirective().getArgument("value").getValue().toString());
         }
         return builder.build();
