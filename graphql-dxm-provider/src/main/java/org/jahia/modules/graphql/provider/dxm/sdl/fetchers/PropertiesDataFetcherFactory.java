f9180ca12b79d6ab66f1138a87ce3cce873f930e
akarmanov <alexandrk_@hotmail.com>
2019-01-22 12:16:51 -0500

    BACKLOG-9444 Add fetchers for object and list types

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/sdl/fetchers/PropertiesDataFetcherFactory.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/sdl/fetchers/PropertiesDataFetcherFactory.java
@@ -1,12 +1,17 @@
 package org.jahia.modules.graphql.provider.dxm.sdl.fetchers;
 
-import graphql.schema.DataFetcher;
-import graphql.schema.GraphQLArgument;
-import graphql.schema.GraphQLDirective;
-import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.*;
 import org.jahia.api.Constants;
 import org.jahia.modules.graphql.provider.dxm.node.GqlJcrNode;
+import org.jahia.modules.graphql.provider.dxm.node.GqlJcrNodeImpl;
 import org.jahia.modules.graphql.provider.dxm.sdl.SDLConstants;
+import org.jahia.services.content.JCRContentUtils;
+import org.jahia.services.content.JCRNodeWrapper;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class PropertiesDataFetcherFactory {
 
@@ -28,6 +33,41 @@ public class PropertiesDataFetcherFactory {
                     };
                 } else if (propertyValue.startsWith(Constants.JCR_CONTENT) && propertyValue.contains(".")) {
                     return new FileContentFetcher(field, propertyValue.split("\\.")[1]);
+                } else if (graphQLFieldDefinition.getType() instanceof GraphQLObjectType) {
+                    return environment -> {
+                        GqlJcrNode node = environment.getSource();
+                        JCRNodeWrapper jcrNode = node.getNode();
+                        try {
+                            JCRNodeWrapper subNode = jcrNode.getNode(field.getProperty());
+                            return new GqlJcrNodeImpl(subNode);
+                        }
+                        catch (RepositoryException e) {
+                            return null;
+                        }
+                    };
+                } else if (graphQLFieldDefinition.getType() instanceof GraphQLList) {
+                    return environment -> {
+                        GqlJcrNode node = environment.getSource();
+                        JCRNodeWrapper jcrNode = node.getNode();
+                        GraphQLObjectType type = (GraphQLObjectType) ((GraphQLList) environment.getFieldDefinition().getType()).getWrappedType();
+                        GraphQLDirective mappingDirective = type.getDirective(SDLConstants.MAPPING_DIRECTIVE);
+                        if (mappingDirective != null) {
+                            GraphQLArgument nodeProperty = mappingDirective.getArgument(SDLConstants.MAPPING_DIRECTIVE_NODE);
+                            if (nodeProperty != null) {
+                                try {
+                                    JCRNodeWrapper subNode = jcrNode.getNode(field.getProperty());
+                                    String nodeType = nodeProperty.getValue().toString();
+                                    return JCRContentUtils.getChildrenOfType(subNode, nodeType).stream()
+                                            .map(GqlJcrNodeImpl::new)
+                                            .collect(Collectors.toList());
+                                }
+                                catch (RepositoryException e) {
+                                    //Do nothing, return empty list below
+                                }
+                            }
+                        }
+                        return Collections.EMPTY_LIST;
+                    };
                 }
             }
         }
