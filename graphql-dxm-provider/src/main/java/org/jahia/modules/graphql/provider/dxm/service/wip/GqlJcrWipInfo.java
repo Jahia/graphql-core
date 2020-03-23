e1141a9d00d1a8d582b7278cecf06d249a971ea1
David Griffon <dgriffon@jahia.com>
2020-03-23 10:30:16 +0100

    BACKLOG-12841 : add WIP query jcr node field and mutation

---
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/service/wip/GqlJcrWipInfo.java
@@ -0,0 +1,93 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2020 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.modules.graphql.provider.dxm.service.wip;
+
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+
+import java.util.Collection;
+
+/**
+ * GraphQL Work in progress informations.
+ */
+@GraphQLName("wipInfo")
+public class GqlJcrWipInfo {
+
+    private WipStatus status;
+    private Collection<String> languages;
+
+    public GqlJcrWipInfo(@GraphQLName("status") WipStatus status, @GraphQLName("languages") Collection<String> languages) {
+        this.status = status;
+        this.languages = languages;
+    }
+
+    @GraphQLField
+    @GraphQLName("status")
+    @GraphQLDescription("Get WIP status")
+    public WipStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * Returns the language of the content object to which the vanity URL maps to.
+     *
+     * @return language of the mapping
+     */
+    @GraphQLField
+    @GraphQLName("languages")
+    @GraphQLDescription("The languages set for Work in progress")
+    public Collection<String> getLanguages() {
+        return languages;
+    }
+
+    public enum WipStatus {
+        @GraphQLDescription("Work in progress disabled")
+        DISABLED,
+        @GraphQLDescription("Work in progress for all languages")
+        ALL_CONTENT,
+        @GraphQLDescription("Work in progress for specified languages")
+        LANGUAGES
+    }
+}
