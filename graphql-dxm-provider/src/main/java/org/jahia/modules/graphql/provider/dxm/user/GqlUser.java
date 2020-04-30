b921e1774bcc051e7eeea67cafb967863b429d0d
tdraier <tdraier@jahia.com>
2020-04-30 17:36:51 +0200

    BACKLOG-13556: Added user endpoint

---
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/user/GqlUser.java
@@ -0,0 +1,25 @@
+package org.jahia.modules.graphql.provider.dxm.user;
+
+import graphql.annotations.annotationTypes.*;
+import org.jahia.modules.graphql.provider.dxm.node.SpecializedTypesHandler;
+import org.jahia.services.usermanager.JahiaUser;
+
+@GraphQLName("User")
+@GraphQLDescription("GraphQL representation of a Jahia user")
+public class GqlUser {
+    private JahiaUser user;
+
+    public GqlUser(JahiaUser jahiaUser) {
+        this.user = jahiaUser;
+    }
+
+    @GraphQLField
+    public String getName() {
+        return user.getName();
+    }
+
+    @GraphQLField
+    public String getProperty(@GraphQLName("name") @GraphQLNonNull @GraphQLDescription("The name of the property") String name) {
+        return user.getProperty(name);
+    }
+}
