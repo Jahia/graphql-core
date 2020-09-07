ff1f6012edfd2d1e1d7493ba75414225d7a921a8
Yousria <ybenchadi@jahia.com>
2020-09-07 11:07:11 +0200

    BACKLOG-14530 : move tools to /tools/developer and adapt path in graphql-playground-route

--- graphql-dxm-provider/src/main/resources/javascript/apps/graphql-playground-route.js
+++ graphql-dxm-provider/src/main/resources/javascript/apps/graphql-playground-route.js
@@ -1,9 +1,10 @@
 window.jahia.i18n.loadNamespaces('graphql-dxm-provider');
 
-window.jahia.uiExtender.registry.add('adminRoute', 'graphiql', {
+window.jahia.uiExtender.registry.add('adminRoute', 'graphql-playground', {
     targets: ['developerTools:20'],
+    requiredPermission: 'developerToolsAccess',
     icon: window.jahia.moonstone.toIconComponent('GraphQl'),
     label: 'graphql-dxm-provider:graphql',
     isSelectable: true,
-    iframeUrl: window.contextJsParameters.contextPath + '/modules/graphql-dxm-provider/tools/graphql-playground.jsp'
+    iframeUrl: window.contextJsParameters.contextPath + '/modules/graphql-dxm-provider/tools/developer/graphql-playground.jsp'
 });
