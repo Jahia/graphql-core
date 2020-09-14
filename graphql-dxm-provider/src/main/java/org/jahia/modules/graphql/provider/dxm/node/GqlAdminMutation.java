2217421c891846b43b4a6da4e303a961e6053258
Anton Kovunov <anton.kovunov@gmail.com>
2020-09-14 12:40:19 -0400

    BACKLOG-14143 added admin mutation and query in provider + modified gitignore

---
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/node/GqlAdminMutation.java
@@ -0,0 +1,13 @@
+package org.jahia.modules.graphql.provider.dxm.node;
+
+
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLName;
+
+/**
+ * GraphQL root object for Admin related mutations.
+ */
+@GraphQLName("adminMutation")
+@GraphQLDescription("Admin mutations")
+public class GqlAdminMutation {
+}
