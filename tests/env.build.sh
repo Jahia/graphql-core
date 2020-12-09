5707d8517f1c36fce4080b0b36b003cbec8ac74f
Michael de Checchi <mdechecchi@jahia.com>
2020-12-09 18:34:44 +0100

    [BACKLOG-15054] Merge prepare scripts into env.build and clean the variables

--- tests/env.build.sh
+++ tests/env.build.sh
@@ -5,6 +5,34 @@
 # It assumes that you previously built the module you're going to be testing
 #   and that the modules artifacts are located one level up
 
+# Copy the root POM
+if [[ -e ../pom.xml ]]; then
+	cp ../pom.xml ./pom.xml
+fi
+
+# Copy the Maven settings
+if [ ! -d ./.circleci ]; then
+	mkdir ./.circleci
+fi
+cp -R ../.circleci/.circleci.settings.xml ./.circleci/.circleci.settings.xml
+
+# Copy subprojects
+if [ ! -d ./graphql-dxm-provider ]; then
+	mkdir ./graphql-dxm-provider
+fi
+cp -R ../graphql-dxm-provider/* ./graphql-dxm-provider/
+
+if [ ! -d ./graphql-extension-example ]; then
+	mkdir ./graphql-extension-example
+fi
+cp -R ../graphql-extension-example/* ./graphql-extension-example/
+
+if [ ! -d ./graphql-test ]; then
+	mkdir ./graphql-test
+fi
+cp -R ../graphql-test/* ./graphql-test/
+
+# Copy the artifacts previously build locally by the CI tool
 if [ ! -d ./artifacts ]; then
   mkdir -p ./artifacts
 fi
