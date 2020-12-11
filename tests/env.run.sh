a40129e9433e97c3ac746479778b541289daf870
Michael de Checchi <mdechecchi@jahia.com>
2020-12-11 18:50:44 +0100

    [BACKLOG-15054] Update the config use built artifacts and reduce the Maven downloads

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -65,14 +65,14 @@ mkdir /tmp/results/reports
 
 echo "== Run tests =="
 # The additional settings is useful when you have to get dependencies from internal repositories
-mvn -s .circleci/.circleci.settings.xml -Pmodule-integration-tests -Djahia.test.url=${TEST_URL} jahia:test surefire-report:report-only
+mvn -Pmodule-integration-tests -Djahia.test.url=${TEST_URL} jahia:test surefire-report:report-only
 if [[ $? -eq 0 ]]; then
   echo "success" > /tmp/results/test_success
-  cp /tmp/target/surefire-reports/* /tmp/results/reports/
+  cp /tmp/target/site/surefire-report.html /tmp/results/reports/
   exit 0
 else
   echo "failure" > /tmp/results/test_failure
-  cp /tmp/target/surefire-reports/* /tmp/results/reports/
+  cp /tmp/target/site/surefire-report.html /tmp/results/reports/
   exit 1
 fi
 
