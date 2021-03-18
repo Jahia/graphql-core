76d704e1e433e2708667b96f7ce890551e940169
Rafael Fonseca <rfonseca85@yahoo.ca>
2021-03-18 11:34:40 -0400

    Backlog 15726 - Adding more nodes to the admin node in graphql (#96)

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlAdminQuery.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlAdminQuery.java
@@ -4,7 +4,16 @@ import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLNonNull;
+import org.apache.jackrabbit.util.ISO8601;
+import org.jahia.api.Constants;
 import org.jahia.bin.Jahia;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * GraphQL root object for Admin related queries.
@@ -12,6 +21,13 @@ import org.jahia.bin.Jahia;
 @GraphQLName("adminQuery")
 @GraphQLDescription("Admin queries root")
 public class GqlAdminQuery {
+
+    public static final Logger logger = LoggerFactory.getLogger(GqlAdminQuery.class);
+
+    /**
+     * @deprecated replaced by jahia node
+     */
+    @Deprecated
     @GraphQLField
     @GraphQLName("version")
     @GraphQLNonNull
@@ -19,4 +35,45 @@ public class GqlAdminQuery {
     public String getProductVersion() {
         return Jahia.getFullProductVersion();
     }
+
+    /**
+     * Get getJahiaVersion
+     *
+     * @return GqlJahiaVersion
+     */
+    @GraphQLField
+    @GraphQLName("jahia")
+    @GraphQLDescription("Version of the running Jahia instance")
+    public GqlJahiaVersion getJahiaVersion()  {
+
+        GqlJahiaVersion gqlJahiaVersion = new GqlJahiaVersion();
+        gqlJahiaVersion.setRelease(Constants.JAHIA_PROJECT_VERSION);
+        gqlJahiaVersion.setBuild(String.valueOf(Jahia.getBuildNumber()));
+        gqlJahiaVersion.setSnapshot(Constants.JAHIA_PROJECT_VERSION.contains("SNAPSHOT"));
+
+        //Formatting buildDate to ISO8601
+        Date date = null;
+        try {
+            date = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, Locale.ENGLISH).parse(Jahia.getBuildDate());
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            gqlJahiaVersion.setBuildDate(ISO8601.format(calendar));
+        } catch (ParseException e) {
+            logger.warn("Exception while parsing build date",e);
+        }
+        return gqlJahiaVersion;
+    }
+
+    /**
+     * Get Build Datetime
+     *
+     * @return String datetime in ISO8601 format
+     */
+    @GraphQLField
+    @GraphQLName("datetime")
+    @GraphQLDescription("Current datetime")
+    public String getDatetime() {
+        return ISO8601.format(Calendar.getInstance());
+    }
+
 }
