d2110c75e8dd3c0e01a1a9b8c0f766f2ed4eca1b
Thomas Draier <tdraier@jahia.com>
2021-05-31 14:39:02 +0200

    export package

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/security/GraphQLFieldWithPermissionRetriever.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/security/GraphQLFieldWithPermissionRetriever.java
@@ -3,9 +3,7 @@ package org.jahia.modules.graphql.provider.dxm.security;
 import graphql.annotations.processor.ProcessingElementsContainer;
 import graphql.annotations.processor.exceptions.GraphQLAnnotationsException;
 import graphql.annotations.processor.retrievers.GraphQLFieldRetriever;
-import graphql.annotations.processor.util.DataFetcherConstructor;
 import graphql.schema.GraphQLFieldDefinition;
-import org.jahia.modules.graphql.provider.dxm.DXGraphQLProvider;
 import org.jahia.modules.graphql.provider.dxm.config.DXGraphQLConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,8 +29,9 @@ public class GraphQLFieldWithPermissionRetriever extends GraphQLFieldRetriever {
         GraphQLRequiresPermission ann = method.getAnnotation(GraphQLRequiresPermission.class);
         GraphQLFieldDefinition definition = graphQLFieldRetriever.getField(method, container);
         if (ann != null) {
-            logger.debug("Adding permission : " + container.getProcessing().peek() + "." + definition.getName() + "=" + ann.value());
-            dxGraphQLConfig.getPermissions().put(container.getProcessing().peek() + "." + definition.getName(), ann.value());
+            String key = container.getProcessing().peek() + "." + definition.getName();
+            logger.debug("Adding permission : {} = {}", key, ann.value());
+            dxGraphQLConfig.getPermissions().put(key, ann.value());
         }
         return definition;
     }
