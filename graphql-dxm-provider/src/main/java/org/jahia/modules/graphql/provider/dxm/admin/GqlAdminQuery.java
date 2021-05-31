056a2bbd346de6d9a0c38767733cccf566b8e245
Thomas Draier <tdraier@jahia.com>
2021-05-31 11:49:45 +0200

    set up permissions

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlAdminQuery.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlAdminQuery.java
@@ -8,17 +8,15 @@ import org.apache.commons.beanutils.MethodUtils;
 import org.apache.jackrabbit.util.ISO8601;
 import org.jahia.api.Constants;
 import org.jahia.bin.Jahia;
+import org.jahia.modules.graphql.provider.dxm.security.GraphQLRequiresPermission;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.DateFormat;
 import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Optional;
-import java.util.Properties;
+import java.util.*;
 
 
 /**
@@ -51,6 +49,7 @@ public class GqlAdminQuery {
     @GraphQLField
     @GraphQLName("jahia")
     @GraphQLDescription("Version of the running Jahia instance")
+    @GraphQLRequiresPermission(value = "admin")
     public GqlJahiaVersion getJahiaVersion()  {
 
         GqlJahiaVersion gqlJahiaVersion = new GqlJahiaVersion();
