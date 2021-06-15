dda3b79a2077bea844359cfec50a336de008061c
Thomas Draier <tdraier@jahia.com>
2021-06-15 10:16:16 +0200

    BACKLOG-16067: Moved admin fields (#124)

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlAdminQuery.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlAdminQuery.java
@@ -5,17 +5,10 @@ import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLNonNull;
 import org.apache.jackrabbit.util.ISO8601;
-import org.jahia.api.Constants;
 import org.jahia.bin.Jahia;
 import org.jahia.modules.graphql.provider.dxm.security.GraphQLRequiresPermission;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.util.*;
-
+import java.util.Calendar;
 
 /**
  * GraphQL root object for Admin related queries.
@@ -24,48 +17,19 @@ import java.util.*;
 @GraphQLDescription("Admin queries root")
 public class GqlAdminQuery {
 
-    public static final Logger logger = LoggerFactory.getLogger(GqlAdminQuery.class);
-    public static String JAHIA_PROJECT_VERSION;
-
     /**
-     * @deprecated replaced by jahia node
+     * Get Jahia admin query
      */
-    @Deprecated
     @GraphQLField
-    @GraphQLName("version")
     @GraphQLNonNull
-    @GraphQLDescription("Version of the running Jahia instance")
-    public String getProductVersion() {
-        return Jahia.getFullProductVersion();
+    @GraphQLDescription("Get Jahia admin query")
+    @GraphQLRequiresPermission(value = "admin")
+    public GqlJahiaAdminQuery getJahia() {
+        return new GqlJahiaAdminQuery();
     }
 
-    /**
-     * Get getJahiaVersion
-     *
-     * @return GqlJahiaVersion
-     */
-    @GraphQLField
-    @GraphQLName("jahia")
-    @GraphQLDescription("Version of the running Jahia instance")
-    @GraphQLRequiresPermission(value = "admin")
-    public GqlJahiaVersion getJahiaVersion()  {
-
-        GqlJahiaVersion gqlJahiaVersion = new GqlJahiaVersion();
-        gqlJahiaVersion.setRelease(Optional.ofNullable(JAHIA_PROJECT_VERSION).orElse(""));
-        gqlJahiaVersion.setBuild(String.valueOf(Jahia.getBuildNumber()));
-        gqlJahiaVersion.setSnapshot(Optional.ofNullable(JAHIA_PROJECT_VERSION).orElse("").contains("SNAPSHOT"));
-
-        //Formatting buildDate to ISO8601
-        Date date = null;
-        try {
-            date = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, Locale.ENGLISH).parse(Jahia.getBuildDate());
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(date);
-            gqlJahiaVersion.setBuildDate(ISO8601.format(calendar));
-        } catch (ParseException e) {
-            logger.warn("Exception while parsing build date",e);
-        }
-        return gqlJahiaVersion;
+    public String getVersion() {
+        return Jahia.getFullProductVersion();
     }
 
     /**
@@ -74,20 +38,8 @@ public class GqlAdminQuery {
      * @return String datetime in ISO8601 format
      */
     @GraphQLField
-    @GraphQLName("datetime")
     @GraphQLDescription("Current datetime")
     public String getDatetime() {
         return ISO8601.format(Calendar.getInstance());
-    }
-
-    static {
-        Properties p = new Properties();
-        try {
-            p.load(Constants.class.getClassLoader().getResourceAsStream("version.properties"));
-        } catch (IOException e) {
-            logger.warn("Exception while retrieving JAHIA_PROJECT_VERSION",e);
-            throw new RuntimeException(e);
-        }
-        JAHIA_PROJECT_VERSION = p.getProperty("version");
     }
 }
