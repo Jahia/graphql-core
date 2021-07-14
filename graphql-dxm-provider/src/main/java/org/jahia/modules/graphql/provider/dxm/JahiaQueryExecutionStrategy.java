a8116200a92173195eff8805b93b1ea02d86bf2a
Geofrey Flores <gflores@jahia.com>
2021-07-14 14:29:43 -0400

    TECH-210: Upgrade to graphql-java 12 (WIP)

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/JahiaQueryExecutionStrategy.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/JahiaQueryExecutionStrategy.java
@@ -3,6 +3,7 @@ package org.jahia.modules.graphql.provider.dxm;
 import graphql.GraphQLError;
 import graphql.execution.*;
 import graphql.language.SourceLocation;
+import org.hibernate.annotations.Fetch;
 
 public class JahiaQueryExecutionStrategy extends AsyncExecutionStrategy {
 
@@ -10,16 +11,15 @@ public class JahiaQueryExecutionStrategy extends AsyncExecutionStrategy {
         super(exceptionHandler);
     }
 
-    @Override
     protected FieldValueInfo completeField(ExecutionContext executionContext, ExecutionStrategyParameters parameters, Object fetchedValue) {
-        FieldValueInfo result = super.completeField(executionContext, parameters, fetchedValue);
+        FieldValueInfo result = super.completeField(executionContext, parameters, (FetchedValue) fetchedValue);
 
         if (fetchedValue instanceof DXGraphQLFieldCompleter && executionContext.getErrors().isEmpty()) {
             // we only complete field if there were no errors on execution
             try {
                 ((DXGraphQLFieldCompleter) fetchedValue).completeField();
             } catch (Exception e) {
-                SourceLocation sourceLocation = (parameters.getField() != null && !parameters.getField().isEmpty()) ? parameters.getField().get(0).getSourceLocation() : null;
+                SourceLocation sourceLocation = parameters.getField().getSingleField().getSourceLocation();
                 GraphQLError error = JahiaDataFetchingExceptionHandler.transformException(e, parameters.getPath(), sourceLocation);
                 executionContext.addError(error, parameters.getPath());
             }
