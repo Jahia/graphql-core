5a9c84e2a3903442a1fb01684a11b6d8d24b2716
Michael de Checchi <mdechecchi@jahia.com>
2021-08-12 17:22:55 +0200

    Add missing set env file

---
+++ graphql-test-docker/set-env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ -f .env ]]; then
+  source .env
+  export $(cat .env | sed 's/=.*//g'| xargs)
+else
+  source .env.example
+  export $(cat .env.example | sed 's/=.*//g'| xargs)
+fi
