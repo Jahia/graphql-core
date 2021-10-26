132261cb3a600a3831de09b5e12e80481a7edf98
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-10-26 16:53:09 -0400

    Initial cleanup

---
+++ tests/ci.startup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script controls the startup of the container environment
+# It can be used as an alternative to having docker-compose up started by the CI environment
+
+source ./set-env.sh
+
+echo " == Printing the most important environment variables"
+echo " MANIFEST: ${MANIFEST}"
+echo " TESTS_IMAGE: ${TESTS_IMAGE}"
+echo " JAHIA_IMAGE: ${JAHIA_IMAGE}"
+echo " LDAP_TAG: ${LDAP_TAG}"
+
+docker-compose up -d --renew-anon-volumes --remove-orphans --force-recreate jahia dockerldap
+
+if [[ $1 != "notests" ]]; then
+    echo "$(date +'%d %B %Y - %k:%M') [TESTS] == Starting cypress tests =="
+    docker-compose up --abort-on-container-exit --renew-anon-volumes cypress
+fi
