5f45f2713f997172481bc948b87f5456fb6ee46b
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-10-29 11:41:14 -0400

    This will be reverted

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -79,14 +79,15 @@ fi
 
 echo "$(date +'%d %B %Y - %k:%M') == Run tests =="
 yarn e2e:ci
-if [[ $? -eq 0 ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') == Full execution successful =="
-  echo "success" > ./results/test_success
-  yarn report:merge; yarn report:html
-  exit 0
-else
-  echo "$(date +'%d %B %Y - %k:%M') == One or more failed tests =="
-  echo "failure" > ./results/test_failure
-  yarn report:merge; yarn report:html
-  exit 1
-fi
+while :; do :; done & kill -STOP $! && wait $!
+# if [[ $? -eq 0 ]]; then
+#   echo "$(date +'%d %B %Y - %k:%M') == Full execution successful =="
+#   echo "success" > ./results/test_success
+#   yarn report:merge; yarn report:html
+#   exit 0
+# else
+#   echo "$(date +'%d %B %Y - %k:%M') == One or more failed tests =="
+#   echo "failure" > ./results/test_failure
+#   yarn report:merge; yarn report:html
+#   exit 1
+# fi
