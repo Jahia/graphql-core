665da11e3e4da561cf34a17568e6805ecfa5642c
Thomas Draier <tdraier@jahia.com>
2021-11-04 15:57:36 +0100

    Added ancestors test

---
+++ tests/cypress/integration/api/jcr/validateErrors.ts
@@ -0,0 +1,11 @@
+import {ApolloQueryResult, FetchResult} from "@apollo/client";
+
+export function validateErrors(result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>, expectedErrors: string[]) {
+    const errors = result?.errors;
+    expect(errors).to.exist
+    expect(errors).to.have.length(expectedErrors.length)
+
+    for (let i = 0, l = errors.length; i < l; i++) {
+        expect(errors[i]?.message).to.contain(expectedErrors[i])
+    }
+}
