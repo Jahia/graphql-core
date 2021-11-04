35c88682945fb5257e442f581ed587ecaf89c61f
Thomas Draier <tdraier@jahia.com>
2021-11-04 15:19:51 +0100

    Fixed script for windows

--- tests/ci.startup.bat
+++ tests/ci.startup.bat
@@ -1,19 +1,12 @@
-#!/bin/bash
-# This script can be used to warmup the environment and execute the tests
-# It is used by the docker image at startup
+rem This script controls the startup of the container environment
 
 call set-env.bat
 
 echo " == Printing the most important environment variables"
-echo " MANIFEST: ${MANIFEST}"
-echo " TESTS_IMAGE: ${TESTS_IMAGE}"
-echo " JAHIA_IMAGE: ${JAHIA_IMAGE}"
-echo " LDAP_TAG: ${LDAP_TAG}"
+echo " MANIFEST: %MANIFEST%"
+echo " TESTS_IMAGE: %TESTS_IMAGE%"
+echo " JAHIA_IMAGE: %JAHIA_IMAGE%"
+echo " LDAP_TAG: %LDAP_TAG%"
 
 docker-compose pull jahia dockerldap
 docker-compose up -d --renew-anon-volumes --remove-orphans --force-recreate jahia dockerldap
-
-if [[ $1 != "notests" ]]; then
-    echo "$(date +'%d %B %Y - %k:%M') [TESTS] == Starting cypress tests =="
-    docker-compose up --abort-on-container-exit --renew-anon-volumes cypress
-fi
