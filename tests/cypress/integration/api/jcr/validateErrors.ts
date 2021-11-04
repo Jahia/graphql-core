3f83616a23d6dc0a9f9a9fe8cdc2dd43580d2f8c
Thomas Draier <tdraier@jahia.com>
2021-11-04 16:07:39 +0100

    validateError

--- tests/cypress/integration/api/jcr/validateErrors.ts
+++ tests/cypress/integration/api/jcr/validateErrors.ts
@@ -1,5 +1,9 @@
 import {ApolloQueryResult, FetchResult} from "@apollo/client";
 
+export function validateError(result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>, expectedError: string) {
+    validateErrors(result, [expectedError])
+}
+
 export function validateErrors(result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>, expectedErrors: string[]) {
     const errors = result?.errors;
     expect(errors).to.exist
