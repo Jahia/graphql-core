0eee5fd5978d0f2f1c02af126c29135eb3f9b501
Thomas Draier <tdraier@jahia.com>
2021-11-04 17:55:52 +0100

    BACKLOG-16497: Added ancestors test (#196)

---
+++ tests/cypress/integration/api/jcr/validateErrors.ts
@@ -0,0 +1,15 @@
+import {ApolloQueryResult, FetchResult} from "@apollo/client";
+
+export function validateError(result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>, expectedError: string) {
+    validateErrors(result, [expectedError])
+}
+
+export function validateErrors(result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>, expectedErrors: string[]) {
+    const errors = result?.errors;
+    expect(errors).to.exist
+    expect(errors).to.have.length(expectedErrors.length)
+
+    for (let i = 0, l = errors.length; i < l; i++) {
+        expect(errors[i]?.message).to.contain(expectedErrors[i])
+    }
+}
---
+++ tests/cypress/integration/api/jcr/validateNode.ts
@@ -0,0 +1,12 @@
+export function validateNode(node, expectedName: string, expectedUuid?: string, expectedPath?: string, expectedParentNodePath?: string) {
+    expect(node).to.have.property('name',expectedName)
+    if (expectedUuid) {
+        expect(node).to.have.property('uuid', expectedUuid)
+    }
+    if (expectedPath) {
+        expect(node).to.have.property('path', expectedPath)
+    }
+    if (expectedParentNodePath) {
+        expect(node).to.have.property('parent.path', expectedParentNodePath)
+    }
+}
