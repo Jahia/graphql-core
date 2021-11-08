27f70128eae08c8a0fc73bd5e4747339f87ff13c
Michael de Checchi <mdechecchi@jahia.com>
2021-11-08 15:15:02 +0100

    Update lint config

--- tests/cypress/integration/api/jcr/validateErrors.ts
+++ tests/cypress/integration/api/jcr/validateErrors.ts
@@ -1,11 +1,17 @@
-import {ApolloQueryResult, FetchResult} from "@apollo/client";
+import { ApolloQueryResult, FetchResult } from '@apollo/client'
 
-export function validateError(result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>, expectedError: string) {
+export function validateError(
+    result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>,
+    expectedError: string,
+) {
     validateErrors(result, [expectedError])
 }
 
-export function validateErrors(result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>, expectedErrors: string[]) {
-    const errors = result?.errors;
+export function validateErrors(
+    result: ApolloQueryResult<any> | FetchResult<{ [p: string]: any }, Record<string, any>, Record<string, any>>,
+    expectedErrors: string[],
+) {
+    const errors = result?.errors
     expect(errors).to.exist
     expect(errors).to.have.length(expectedErrors.length)
 
