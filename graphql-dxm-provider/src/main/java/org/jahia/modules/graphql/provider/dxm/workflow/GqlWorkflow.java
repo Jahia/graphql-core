7d524817d93d98c173680247283f0c704021fa8d
Thomas Draier <tdraier@jahia.com>
2022-01-26 09:33:26 +0100

    workflows

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/workflow/GqlWorkflow.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/workflow/GqlWorkflow.java
@@ -17,7 +17,10 @@ package org.jahia.modules.graphql.provider.dxm.workflow;
 
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
+import org.apache.jackrabbit.util.ISO8601;
 import org.jahia.services.workflow.Workflow;
+
+import java.util.Calendar;
 
 @GraphQLName("Workflow")
 public class GqlWorkflow {
@@ -32,4 +35,22 @@ public class GqlWorkflow {
     public String getStartUser() {
         return workflowProcess.getStartUser();
     }
+
+    @GraphQLField
+    public String getCreationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(workflowProcess.getStartTime());
+        return ISO8601.format(calendar);
+    }
+
+    @GraphQLField
+    public String getDueDate() {
+        if (workflowProcess.getDuedate() != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(workflowProcess.getDuedate());
+            return ISO8601.format(calendar);
+        }
+        return null;
+    }
+
 }
