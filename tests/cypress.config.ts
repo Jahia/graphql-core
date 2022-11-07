2bc0dd66152120a45303fb1a945f238ab67c4f7f
Alex Karmanov <alexandrk_@hotmail.com>
2022-11-07 10:08:27 -0500

    BACKLOG-20253 Update cypress to v10

---
+++ tests/cypress.config.ts
@@ -0,0 +1,25 @@
+import {defineConfig} from 'cypress';
+
+export default defineConfig({
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    videoUploadOnPasses: false,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+        configFile: 'reporter-config.json'
+    },
+    screenshotsFolder: './results/screenshots',
+    videosFolder: './results/videos',
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            return require('./cypress/plugins/index.js')(on, config);
+        },
+        excludeSpecPattern: '*.ignore.ts',
+        baseUrl: 'http://localhost:8080',
+        experimentalSessionAndOrigin: false
+    }
+});
