b21a94e525d21062664a4818e3eea84aa4f3b12b
jsinovassin <jsinovassinnaik@jahia.com>
2022-11-21 17:34:13 +0100

    TECH-971 : add endpoint to access background jobs

---
+++ tests/cypress/e2e/api/admin/jobs.cy.ts
@@ -0,0 +1,25 @@
+import gql from 'graphql-tag'
+
+describe('Jobs graphQL endpoint', () => {
+    it('background job list should not be empty', function () {
+        cy.apollo({
+            query: gql`
+                query {
+                    admin {
+                        jahia {
+                            scheduler {
+                                jobs {
+                                    group
+                                    name
+                                }
+                            }
+                        }
+                    }
+                }
+            `,
+        }).should((response) => {
+            const result = response.data.admin.jahia.scheduler.jobs
+            expect(result.length).to.gt(0)
+        })
+    })
+})
