1f09597fdf1396499ebe36d46c8de66a70b70c1c
Francois G <fgerthoffert@jahia.com>
2023-02-02 16:27:45 +0100

    Added nodes to fetch database information

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlJahiaAdminQuery.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/admin/GqlJahiaAdminQuery.java
@@ -25,15 +25,18 @@ import org.jahia.api.Constants;
 import org.jahia.bin.Jahia;
 import org.jahia.modules.graphql.provider.dxm.osgiconfig.GqlConfigurationQuery;
 import org.jahia.modules.graphql.provider.dxm.scheduler.GqlScheduler;
+import org.jahia.utils.DatabaseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.*;
-
 
 /**
  * GraphQL root object for Admin related queries.
@@ -84,6 +87,35 @@ public class GqlJahiaAdminQuery {
             logger.warn("Exception while parsing build date", e);
         }
         return gqlJahiaVersion;
+    }
+
+    /**
+     * Get getJahiaDatabase
+     *
+     * @return GqlJahiaDatabase
+     * @throws SQLException
+     */
+    @GraphQLField
+    @GraphQLDescription("Details about the database Jahia is connected to")
+    public GqlJahiaDatabase getDatabase() throws SQLException {
+        GqlJahiaDatabase gqlJahiaDatabase = new GqlJahiaDatabase();
+        gqlJahiaDatabase.setType(DatabaseUtils.getDatabaseType().toString());
+        Connection connection = null;
+        try {
+            connection = DatabaseUtils.getDatasource().getConnection();
+            DatabaseMetaData metadata = connection.getMetaData();
+        
+            gqlJahiaDatabase.setName(metadata.getDatabaseProductName());
+            gqlJahiaDatabase.setVersion(metadata.getDatabaseProductVersion());
+            gqlJahiaDatabase.setDriverName(metadata.getDriverName());
+            gqlJahiaDatabase.setDriverVersion(metadata.getDriverVersion());
+            gqlJahiaDatabase.setUrl(metadata.getURL());
+        } catch (Exception e) {
+            logger.error("Unable to get database information . Cause: " + e.getMessage(), e);
+        } finally {
+            DatabaseUtils.closeQuietly(connection);
+        }
+        return gqlJahiaDatabase;
     }
 
     /**
