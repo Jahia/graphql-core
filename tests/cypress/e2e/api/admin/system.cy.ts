6edf5f26823890555b66377ea7bbd9c029b6d16a
Francois G <fgerthoffert@jahia.com>
2023-02-02 20:12:44 +0100

    Fixed linting issues

--- tests/cypress/e2e/api/admin/system.cy.ts
+++ tests/cypress/e2e/api/admin/system.cy.ts
@@ -5,16 +5,16 @@ describe('Test admin jahia cluster endpoint', () => {
         const systemKey = 'karaf.base'
         cy.apollo({
             queryFile: 'admin/system.graphql',
-            variables: { systemKey: systemKey},
+            variables: { systemKey: systemKey },
         }).should((response: any) => {
             expect(response.data.admin.jahia.system.property.key).to.equal(systemKey)
-            expect(response.data.admin.jahia.system.property.value).to.equal("/var/jahia/karaf")
+            expect(response.data.admin.jahia.system.property.value).to.equal('/var/jahia/karaf')
 
-            expect(response.data.admin.jahia.system.os.name).to.equal("Linux")
+            expect(response.data.admin.jahia.system.os.name).to.equal('Linux')
             expect(response.data.admin.jahia.system.os.architecture.length).to.greaterThan(3)
             expect(response.data.admin.jahia.system.os.version.length).to.greaterThan(3)
 
-            expect(response.data.admin.jahia.system.java.runtimeName).to.equal("OpenJDK Runtime Environment")
+            expect(response.data.admin.jahia.system.java.runtimeName).to.equal('OpenJDK Runtime Environment')
             expect(response.data.admin.jahia.system.java.runtimeVersion.length).to.greaterThan(3)
             expect(response.data.admin.jahia.system.java.vendor.length).to.greaterThan(3)
             expect(response.data.admin.jahia.system.java.vendorVersion.length).to.greaterThan(3)
