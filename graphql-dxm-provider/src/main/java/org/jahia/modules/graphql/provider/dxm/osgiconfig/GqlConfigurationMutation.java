04b618116e20d10a6d733f63a615cfe153df686d
Thomas Draier <tdraier@jahia.com>
2023-08-16 16:49:11 +0200

    Add 'updateOnly' parameter for configuration edition (#334)

--- graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/osgiconfig/GqlConfigurationMutation.java
+++ graphql-dxm-provider/src/main/java/org/jahia/modules/graphql/provider/dxm/osgiconfig/GqlConfigurationMutation.java
@@ -16,14 +16,11 @@
 package org.jahia.modules.graphql.provider.dxm.osgiconfig;
 
 import graphql.annotations.annotationTypes.GraphQLDescription;
-import graphql.annotations.annotationTypes.GraphQLField;
-import graphql.annotations.annotationTypes.GraphQLName;
 import org.jahia.modules.graphql.provider.dxm.DXGraphQLFieldCompleter;
 import org.jahia.modules.graphql.provider.dxm.DataFetchingException;
 import org.jahia.modules.graphql.provider.dxm.osgi.annotations.GraphQLOsgiService;
 import org.jahia.services.modulemanager.spi.Config;
 import org.jahia.services.modulemanager.spi.ConfigService;
-import org.jahia.services.modulemanager.util.PropertiesList;
 import org.jahia.services.modulemanager.util.PropertiesValues;
 
 import javax.inject.Inject;
@@ -31,41 +28,23 @@ import java.io.IOException;
 
 @GraphQLDescription("Mutation for OSGi configuration")
 public class GqlConfigurationMutation extends GqlValueMutation implements DXGraphQLFieldCompleter {
-
-    private final String pid;
-    private final String identifier;
+    private Config configuration = null;
 
     @Inject
     @GraphQLOsgiService
     private ConfigService configService;
 
-    private Config configuration = null;
-
-    public GqlConfigurationMutation(String pid, String identifier) {
+    public GqlConfigurationMutation(Config configuration) {
         super(null);
-        this.pid = pid;
-        this.identifier = identifier;
+        this.configuration = configuration;
     }
 
     @Override
     protected PropertiesValues getPropertiesValues() {
         if (propertiesValues == null) {
-            propertiesValues = getConfiguration().getValues();
+            propertiesValues = configuration.getValues();
         }
         return super.getPropertiesValues();
-    }
-
-    private Config getConfiguration() {
-        try {
-            if (identifier == null) {
-                configuration = configService.getConfig(pid);
-            } else {
-                configuration = configService.getConfig(pid, identifier);
-            }
-        } catch (IOException e) {
-            throw new DataFetchingException(e);
-        }
-        return configuration;
     }
 
     @Override
