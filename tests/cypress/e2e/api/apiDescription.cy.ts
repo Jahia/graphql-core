c726737c94cf57e3617006f2932f1447b6c34589
Francois G <fgerthoffert@jahia.com>
2024-02-08 10:31:23 +0100

    TECH-1631: Moved the logic of checking the description to jahia-cypress (#393)

--- tests/cypress/e2e/api/apiDescription.cy.ts
+++ tests/cypress/e2e/api/apiDescription.cy.ts
@@ -1,213 +1,64 @@
-import gql from 'graphql-tag';
+/* eslint max-nested-callbacks: ["error", 6] */
 
-describe('Test if every type in graphQL API has description', () => {
-    it('Check every input for the User Type', () => {
-        const types = new Set();
-        const noDesc = new Set();
-        const invalidNames = new Set();
+import {getDescriptions} from '@jahia/cypress';
 
-        cy.apolloClient()
-            .then({timeout: 25000}, client => executeTest(client, 'Query', types, noDesc, invalidNames))
-            .should(() => {
-                const noDescBlacklist = [
-                    'type=JCRSite/field=findAvailableNodeName/arg=nodeType',
-                    'type=JCRSite/field=findAvailableNodeName/arg=language',
-                    'type=VanityUrl/field=findAvailableNodeName/arg=nodeType',
-                    'type=VanityUrl/field=findAvailableNodeName/arg=language',
-                    'type=GqlDashboard',
-                    'type=GqlModule',
-                    'type=EditorForm',
-                    'type=EditorFormSection',
-                    'type=EditorFormFieldSet',
-                    'type=EditorFormField',
-                    'type=EditorFormFieldValue',
-                    'type=EditorFormProperty',
-                    'type=EditorFormFieldValueConstraint',
-                    'type=GqlEditorForms',
-                    'type=GqlEditorForm',
-                    'type=GqlEditorFormSection',
-                    'type=GqlEditorFormFieldSet',
-                    'type=GqlEditorFormField',
-                    'type=GqlEditorFormFieldValue',
-                    'type=GqlEditorFormProperty',
-                    'type=GqlEditorFormFieldValueConstraint',
-                    'type=GqlEditorFormValue',
-                    'type=GqlEditorFormValueConstraint',
-                    'type=InputContextEntryInput',
-                    'inputType=InputContextEntryInput/field=key',
-                    'inputType=InputContextEntryInput/field=value',
-                    'type=Query/field=categoryById/arg=id',
-                    'type=Metadata/field=uuid',
-                    'type=Metadata/field=path',
-                    'type=Category/field=uuid',
-                    'type=Category/field=path',
-                    'type=Query/field=categoryByPath/arg=path',
-                    'type=GqlNpmHelper'
-                ];
-                noDescBlacklist.forEach(n => noDesc.delete(n));
+describe('Test for GraphQL schema description', () => {
+    // The blacklist is used to ignore nodes that are not registered directly by graphql-core
+    // These descriptions should be added in their respective codebases
+    const noDescBlacklist = [
+        // Missing but provided by: https://github.com/Jahia/npm-modules-engine
+        'GqlNpmHelper',
 
-                const invalidNameBlacklist = ['wipInfo'];
-                invalidNameBlacklist.forEach(n => invalidNames.delete(n));
+        // // Missing but provided by:  https://github.com/Jahia/content-editor or https://github.com/Jahia/jcontent
+        'GqlEditorForms',
+        'GqlEditorForm',
 
-                expect(JSON.stringify(Array.from(noDesc))).to.equals('[]');
-                expect(JSON.stringify(Array.from(invalidNames))).to.equals('[]');
+        // Missing but provided by: https://github.com/Jahia/jahia-dashboard
+        'GqlDashboard',
+
+        // Missing but provided by: https://github.com/Jahia/server-availability-manager
+        'AdminQuery/JahiaAdminQuery/healthCheck',
+        'AdminQuery/JahiaAdminQuery/GqlHealthCheck',
+        'AdminQuery/JahiaAdminQuery/Load',
+
+        // Missing but provided by: https://github.com/Jahia/security-filter-tools
+        'JWTToken',
+        'jwtToken',
+
+        // These are provided by graphql-dxm-provider
+        // Descriptions should be added via a separate ticket: BACKLOG-22338
+        'Query/categoryById/id',
+        'Query/categoryByPath/path',
+        'findAvailableNodeName',
+        'MulticriteriaEvaluation',
+        'SortType',
+        'WipStatus',
+        'Mutation/AdminMutation/JahiaAdminMutation/GqlConfigurationMutation',
+        'Mutation/JCRMutation',
+        'Mutation/WorkflowMutation',
+        'Mutation/mutateWorkflows',
+        'Subscription/workflowEvent/GqlWorkflowEvent',
+        'Subscription/backgroundJobSubscription'
+    ];
+
+    const entryNodes = ['Query', 'Mutation', 'Subscription'];
+    entryNodes.forEach(entryNode => {
+        it(`Description for all nodes under ${entryNode}`, () => {
+            getDescriptions(entryNode).then(result => {
+                console.log(result);
+
+                // Get the list of nodes that are missing descriptions
+                // Remove the nodes that are in the blacklist
+                const noDesc = result
+                    .filter((graphqlType => graphqlType.description === null || graphqlType.description.length === 0))
+                    .filter((graphqlType => !noDescBlacklist.some(t => graphqlType.nodePath.join('/').includes(t))));
+
+                noDesc.forEach(description => {
+                    cy.log(`Missing description for ${description.schemaType} at path: ${description.nodePath.join('/')}`);
+                    console.log(`Missing description for ${description.schemaType} at path: ${description.nodePath.join('/')}`);
+                });
+                cy.then(() => expect(noDesc.length).to.equal(0));
             });
+        });
     });
 });
-
-// Test to go down the AST of GraphQL to check for descriptions
-// eslint-disable-next-line max-params
-const executeTest = async (client, typeName, types, noDesc, invalidNames) => {
-    if (types.has(typeName)) {
-        return;
-    }
-
-    if (typeName[0] !== typeName[0].toUpperCase()) {
-        invalidNames.add(typeName);
-    }
-
-    types.add(typeName);
-
-    const query = constructQuery(typeName);
-    const response = await client.query({query});
-    const responseDataType = response.data.__type;
-    if (responseDataType === null || responseDataType === undefined || responseDataType.kind === 'UNION') {
-        return;
-    }
-
-    if (!responseDataType.description) {
-        noDesc.add('type=' + responseDataType.name);
-    }
-
-    if (responseDataType.fields) {
-        await asyncForEach(responseDataType.fields, async field => {
-            if (field.args) {
-                await asyncForEach(field.args, async arg => {
-                    await fieldCheck(
-                        client,
-                        'type=' + responseDataType.name + '/field=' + field.name + '/arg=' + arg.name,
-                        arg,
-                        types,
-                        noDesc,
-                        invalidNames
-                    );
-                });
-            }
-
-            await fieldCheck(
-                client,
-                'type=' + responseDataType.name + '/field=' + field.name,
-                field,
-                types,
-                noDesc,
-                invalidNames
-            );
-        });
-    }
-
-    if (responseDataType.inputFields) {
-        await asyncForEach(responseDataType.inputFields, async field => {
-            if (field.args) {
-                await asyncForEach(field.args, async arg => {
-                    await fieldCheck(
-                        client,
-                        'inputType=' + responseDataType.name + '/arg=' + arg.name,
-                        arg,
-                        types,
-                        noDesc,
-                        invalidNames
-                    );
-                });
-            }
-
-            await fieldCheck(
-                client,
-                'inputType=' + responseDataType.name + '/field=' + field.name,
-                field,
-                types,
-                noDesc,
-                invalidNames
-            );
-        });
-    }
-};
-
-// eslint-disable-next-line max-params
-const fieldCheck = async (client, message, field, types, noDesc, invalidNames) => {
-    if (field.description === null) {
-        noDesc.add(message);
-    }
-
-    let type = field.type;
-
-    while (type.ofType) {
-        type = type.ofType;
-    }
-
-    if (type.kind === 'OBJECT' || type.kind === 'INPUT_OBJECT') {
-        await executeTest(client, type.name, types, noDesc, invalidNames);
-    }
-};
-
-const asyncForEach = async (array, callback) => {
-    for (let index = 0; index < array.length; index++) {
-        // eslint-disable-next-line no-await-in-loop
-        await callback(array[index], index, array);
-    }
-};
-
-const constructQuery = typeName => {
-    return gql`query IntrospectionQuery {
-        __type(name:"${typeName}") {
-            kind
-            name
-            description
-            fields {
-                name
-                description
-                args {
-                    name
-                    description
-                    type {
-                        kind
-                        name
-                        description
-                        ofType {
-                            kind
-                            name
-                            description
-                        }
-                    }
-                }
-                type {
-                    kind
-                    name
-                    description
-                    ofType {
-                        kind
-                        name
-                        description
-                    }
-                }
-            }
-            inputFields {
-                name
-                description
-                type {
-                    kind
-                    name
-                    description
-                    ofType {
-                        kind
-                        name
-                        description
-                        ofType {
-                            kind
-                            name
-                        }
-                    }
-                }
-            }
-        }
-    }`;
-};
