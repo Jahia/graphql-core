f0218db1bdfe3ee7c9a77c6506cab73344898cd8
Michael Krasnov <mihalKrasnov@gmail.com>
2024-05-09 16:24:34 -0400

    TECH-655: Migrated GQL reference tests to cypress (#433)

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     videosFolder: './results/videos',
     viewportWidth: 1366,
     viewportHeight: 768,
+    watchForFileChanges: false,
     e2e: {
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
