2e3fbdf73194f8baf664e52fe85a7e6043d91d74
Geofrey Flores <gflores@jahia.com>
2024-08-23 16:47:53 -0400

    SEC-104: Fix SDL extensions test

--- tests/cypress/e2e/sdl/sdlExtensions.cy.js
+++ tests/cypress/e2e/sdl/sdlExtensions.cy.js
@@ -1,32 +1,30 @@
-import {
-    getCoreSdlExtensions,
-    getExampleSdlExtensions
-} from '../../fixtures/sdl/sdlExtensions';
+import {getTypeFields} from '../../fixtures/sdl/sdlExtensions';
 
 describe('SDL extensions', () => {
+
+    function verifySdlFields(typeName, expectedFields) {
+        return cy.apollo({query: getTypeFields(typeName)})
+            .then((response) => {
+                const data = response?.data || {};
+                const fieldNames = data['__type']?.fields?.map(f => f.name) || [];
+                if (expectedFields) {
+                    expect(fieldNames).to.deep.eq(expectedFields);
+                }
+                return fieldNames;
+            });
+    }
+
     it('should have SDL extensions defined in dxm-provider module', () => {
-        cy.apollo({query: getCoreSdlExtensions}).then((response) => {
-            const {category, imageAsset} = response?.data || {};
-            expect(category?.fields?.map(f => f.name)).to.deep.eq(
-                ['metadata', 'description', 'title']
-            );
-            expect(imageAsset?.fields?.map(f => f.name)).to.deep.eq(
-                ['metadata', 'type', 'size', 'height', 'width']
-            );
-        });
+        verifySdlFields('Category', ['metadata', 'description', 'title']);
+        verifySdlFields('ImageAsset', ['metadata', 'type', 'size', 'height', 'width']);
     });
 
     it('should have SDL extensions defined in extension-examples module', () => {
-        cy.apollo({query: getExampleSdlExtensions}).then((response) => {
-            const {newsSdl, images, queries} = response?.data || {};
-            expect(newsSdl?.fields?.map(f => f.name)).to.deep.eq(
-                ['title', 'description', 'checked', 'date']
-            );
-            expect(images?.fields?.map(f => f.name)).to.deep.eq(
-                ['height']
-            );
+        verifySdlFields('NewsSDL', ['title', 'description', 'checked', 'date']);
+        verifySdlFields('Images', ['height']);
+        verifySdlFields('Query').then(returnedFields => {
             ['newsByChecked', 'myNewsByDate', 'myImagesByHeight'].forEach(queryField => {
-                expect(queries?.fields?.find(f => f.name === queryField)).to.exist;
+                expect(returnedFields).to.include(queryField);
             });
         });
     });
--- tests/cypress/fixtures/sdl/sdlExtensions.js
+++ tests/cypress/fixtures/sdl/sdlExtensions.js
@@ -1,25 +1,8 @@
 import gql from 'graphql-tag';
 
-export const getCoreSdlExtensions = gql`
+export const getTypeFields = (typeName) => gql`
     query coreSdlExtensions {
-        category: __type(name: "Category") {
-            fields {name}
-        },
-        imageAsset: __type(name: "ImageAsset") {
-            fields {name}
-        }
-    }
-`;
-
-export const getExampleSdlExtensions = gql`
-    query exampleSdlExtensions {
-        newsSdl: __type(name:"NewsSDL") {
-            fields {name}
-        }
-        images: __type(name:"Images") {
-            fields {name}
-        }
-        queries: __type(name: "Query") {
+        __type(name: "${typeName}") {
             fields {name}
         }
     }
