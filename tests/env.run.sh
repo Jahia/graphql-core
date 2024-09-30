9570087d5db6895da71b774ed2c460563efe574e
Francois G. <fgerthoffert@jahia.com>
2024-09-30 10:31:26 +0200

    BACKLOG-23119: Updated the CI to support testing of release artifacts in staging (#459)

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-npx --yes --package @jahia/cypress env.run
-
+version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
+echo Using @jahia/cypress@$version...
+npx --yes --package @jahia/cypress@$version env.run
