0297a28aa4b18621110665938066d9b5ce7088a0
Francois G. <fgerthoffert@jahia.com>
2025-01-28 10:59:29 +0100

    Fixed linting

--- tests/cypress/e2e/api/admin/system.cy.ts
+++ tests/cypress/e2e/api/admin/system.cy.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { getJahiaVersion } from '@jahia/cypress'
-import { compare } from 'compare-versions'
+/* eslint max-nested-callbacks: ["error", 5] */
+import {getJahiaVersion} from '@jahia/cypress';
+import {compare} from 'compare-versions';
 
 describe('Test admin jahia system endpoint', () => {
     it('Gets system details', () => {
@@ -11,16 +12,16 @@ describe('Test admin jahia system endpoint', () => {
             expect(response.data.admin.jahia.system.os.architecture.length).to.greaterThan(3);
             expect(response.data.admin.jahia.system.os.version.length).to.greaterThan(3);
 
-            getJahiaVersion().then((jahiaVersion) => {
+            getJahiaVersion().then(jahiaVersion => {
                 cy.log(JSON.stringify(jahiaVersion)).then(() => {
                     if (compare(jahiaVersion.release.replace('-SNAPSHOT', ''), '8.2.1', '<')) {
                         expect(response.data.admin.jahia.system.java.runtimeName).to.equal('OpenJDK Runtime Environment');
                     } else {
                         // With TECH-2029 the GraalVM vendor was changed, resulting in a new runtime name
                         expect(response.data.admin.jahia.system.java.runtimeName).to.equal('Java(TM) SE Runtime Environment');
-                    }                    
-                })
-            })
+                    }
+                });
+            });
             expect(response.data.admin.jahia.system.java.runtimeVersion.length).to.greaterThan(3);
             expect(response.data.admin.jahia.system.java.vendor.length).to.greaterThan(3);
             expect(response.data.admin.jahia.system.java.vendorVersion.length).to.greaterThan(3);
