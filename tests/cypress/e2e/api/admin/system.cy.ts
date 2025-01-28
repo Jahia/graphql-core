438f334599222515151112c4ac7e29f20a622fc5
Francois G. <fgerthoffert@jahia.com>
2025-01-28 02:52:32 -0500

    TECH-2029: Fixed a compatibility issue with the runtime name

--- tests/cypress/e2e/api/admin/system.cy.ts
+++ tests/cypress/e2e/api/admin/system.cy.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { getJahiaVersion } from '@jahia/cypress'
 
 describe('Test admin jahia cluster endpoint', () => {
     it('Gets cluster details', () => {
@@ -9,7 +10,15 @@ describe('Test admin jahia cluster endpoint', () => {
             expect(response.data.admin.jahia.system.os.architecture.length).to.greaterThan(3);
             expect(response.data.admin.jahia.system.os.version.length).to.greaterThan(3);
 
-            expect(response.data.admin.jahia.system.java.runtimeName).to.equal('Java(TM) SE Runtime Environment');
+            getJahiaVersion().then((jahiaVersion) => {
+                console.log(jahiaVersion)
+                if (compare(jahiaVersion.release.replace('-SNAPSHOT', ''), '8.2.1', '<')) {
+                    expect(response.data.admin.jahia.system.java.runtimeName).to.equal('OpenJDK Runtime Environment');
+                } else {
+                    // With TECH-2029 the GraalVM vendor was changed, resulting in a new runtime name
+                    expect(response.data.admin.jahia.system.java.runtimeName).to.equal('Java(TM) SE Runtime Environment');
+                }
+            })
             expect(response.data.admin.jahia.system.java.runtimeVersion.length).to.greaterThan(3);
             expect(response.data.admin.jahia.system.java.vendor.length).to.greaterThan(3);
             expect(response.data.admin.jahia.system.java.vendorVersion.length).to.greaterThan(3);
