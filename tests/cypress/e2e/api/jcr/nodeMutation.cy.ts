76d8dbacbaa0c56f8b317ed7b5904b9556e4866d
akarmanov <alexandrk_@hotmail.com>
2025-05-14 08:54:09 -0400

    Revert "fix: Add move test"
    This reverts commit 62a76e38e98e5a0cc13e93a4f9ad6fed4873f9b8.

--- tests/cypress/e2e/api/jcr/nodeMutation.cy.ts
+++ tests/cypress/e2e/api/jcr/nodeMutation.cy.ts
@@ -1,5 +1,4 @@
 import gql from 'graphql-tag';
-import {uploadFile} from '@jahia/cypress';
 
 describe('Import content', () => {
     const importContentGql = gql`
@@ -9,40 +8,6 @@ describe('Import content', () => {
             }
         }
     `;
-
-    const vfsMount = gql`
-        mutation vfsMutation {
-            admin {
-                mountPoint {
-                    addVfs(
-                    name: "testVfs"
-                    rootPath: "/home/tomcat"
-                    mountPointRefPath: "/sites/systemsite/files"
-                )
-            }
-          }
-        }
-    `;
-
-    const moveNode = gql`mutation moveNode($pathsOrIds: [String]!, $destParentPathOrId: String!) {
-        jcr {
-            mutateNodes(pathsOrIds: $pathsOrIds) {
-                move(parentPathOrId: $destParentPathOrId, renameOnConflict: true)
-                node {
-                    path
-                }
-            }
-        }
-    }
-    `;
-
-    before(() => {
-        uploadFile('jcr/snowbearHome.jpeg', '/sites/systemsite/files', 'snowbearHome.jpeg', 'image/jpeg').then(() => {
-            cy.apollo({
-                mutation: vfsMount
-            });
-        });
-    });
 
     it('should reject zip bomb', () => {
         cy.fixture('jcr/zbsm.zip', 'binary').then(zipBomb => {
@@ -55,18 +20,6 @@ describe('Import content', () => {
                 console.log(JSON.stringify(result));
                 expect(result.graphQLErrors[0].message).to.contain('FileSizeLimitExceededException');
             });
-        });
-    });
-
-    it('can move node into a vfs without exception', () => {
-        cy.apollo({
-            mutation: moveNode,
-            variables: {
-                pathsOrIds: ['/sites/systemsite/files/snowbearHome.jpeg'],
-                destParentPathOrId: '/sites/systemsite/files/testVfs'
-            }
-        }).then(result => {
-            expect(JSON.stringify(result)).to.equal('{"data":{"jcr":{"mutateNodes":[{"move":"/sites/systemsite/files/testVfs/snowbearHome.jpeg","node":{"path":"/sites/systemsite/files/testVfs/snowbearHome.jpeg","__typename":"GenericJCRNode"},"__typename":"JCRNodeMutation"}],"__typename":"JCRMutation"}}}');
         });
     });
 });
