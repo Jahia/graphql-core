9c360fca2ae50337f08745b420a6cbe34067f36d
Volodymyr Tereshchuk <vtereshchuk@jahia.com>
2025-12-26 15:53:36 +0100

    test: extend GraphQL UI test

--- tests/cypress/e2e/tools/graphqlWorkspace.cy.ts
+++ tests/cypress/e2e/tools/graphqlWorkspace.cy.ts
@@ -11,9 +11,40 @@ describe('GraphQL Workspace tests', () => {
     });
 
     it('Should be able to open the GraphQL workspace', () => {
+        // Spy on console errors, warnings and messages to make sur UI is loaded without issues.
+        cy.on('window:before:load', window => {
+            cy.spy(window.console, 'error').as('errors');
+            cy.spy(window.console, 'warn').as('warnings');
+            cy.spy(window.console, 'log').as('messages');
+        });
+
         cy.visit(GRAPHQL_WORKSPACE_URL);
         cy.get('[aria-label="Show Documentation Explorer"]').click();
-        // Validate that the JCRNode type, as an example, is present
-        cy.get('a.graphiql-doc-explorer-type-name').contains('JCRNode');
+
+        // Make sure Root Types exist
+        cy.get('div.graphiql-doc-explorer-section-content>div').contains('query: Query');
+        cy.get('div.graphiql-doc-explorer-section-content>div').contains('mutation: Mutation');
+        cy.get('div.graphiql-doc-explorer-section-content>div').contains('subscription: Subscription');
+
+        // Look for "All Schema Types" title,
+        // and then for all links in the sibling div to make sure their list is not empty.
+        // Artificial "magic" number 42 was chosen to make sure list contains meaningful amount of types.
+        // See https://simple.wikipedia.org/wiki/42_(answer) for reference.
+        cy.get('div.graphiql-doc-explorer-section-title')
+            .contains('All Schema Types')
+            .next()
+            .find('a')
+            .should('have.length.greaterThan', 42);
+
+        // Verify errors or warnings are absent in console during loading.
+        // Ensure expected log messages are present.
+        cy.get('@warnings').should('have.callCount', 0);
+        cy.get('@errors').should('have.callCount', 0);
+        cy.get('@messages').then(messages => {
+            const allMessages = messages.getCalls().map(call => call.args.join(' '));
+
+            expect(allMessages.some(msg => msg.toLowerCase().includes('starting')), 'Console should contain "starting"').to.be.true;
+            expect(allMessages.some(msg => msg.toLowerCase().includes('dom loaded')), 'Console should contain "dom loaded"').to.be.true;
+        });
     });
 });
