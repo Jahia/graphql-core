a41228adea29734f72045b5cd3a3e71c8a66897a
Baptiste Grimaud <bgrimaud@jahia.com>
2025-12-26 11:40:19 +0100

    test: Check that GraphQL workspace loads correctly (#561)

---
+++ tests/cypress/e2e/tools/graphqlWorkspace.cy.ts
@@ -0,0 +1,19 @@
+describe('GraphQL Workspace tests', () => {
+    const GRAPHQL_WORKSPACE_URL = '/modules/graphql-dxm-provider/tools/graphql-workspace.jsp';
+    beforeEach('prerequisites', () => {
+        cy.login();
+    });
+
+    it('Should the link to the GraphQL workspace be available on the /tools page', () => {
+        cy.visit('/tools');
+        cy.get(`a[href="${GRAPHQL_WORKSPACE_URL}"]`).should('exist');
+        cy.get(`a[href="${GRAPHQL_WORKSPACE_URL}"]`).contains('Jahia GraphQL Core Provider : graphql-workspace');
+    });
+
+    it('Should be able to open the GraphQL workspace', () => {
+        cy.visit(GRAPHQL_WORKSPACE_URL);
+        cy.get('[aria-label="Show Documentation Explorer"]').click();
+        // Validate that the JCRNode type, as an example, is present
+        cy.get('a.graphiql-doc-explorer-type-name').contains('JCRNode');
+    });
+});
